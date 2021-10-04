1b34f46b835e7e5781d44e75e41e5e745f375a4f
Kiara <kv.silva@campus.fct.unl.pt>
2021-10-04 13:00:49 +0100

    skeletons and strings for new methods

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,17 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_CONTACT  = "GN";
+    public static final String EQUAL_NUMBER  = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
-    public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
-    public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
-    public static final String CONTACT_ADDED = "contactBook.Contact added.";
-    public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
-    public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
-    public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String CONTACT_EXISTS = "Contact already exists.";
+    public static final String NAME_NOT_EXIST = "Contact does not exist.";
+    public static final String CONTACT_ADDED = "Contact added.";
+    public static final String CONTACT_REMOVED = "Contact removed.";
+    public static final String CONTACT_UPDATED = "Contact updated.";
+    public static final String BOOK_EMPTY = "Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +54,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GET_CONTACT:
+                    getContact(cBook);
+                    break;
+                case EQUAL_NUMBER:
+                    checkSameNumber(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +154,13 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    //new methods
+
+    private static void checkSameNumber(ContactBook cBook) {
+    }
+
+    private static void getContact(ContactBook cBook) {
     }
 }
